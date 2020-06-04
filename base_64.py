4c0bc994fca08a3a80735bd09d84a1cc550d5410
johndoe-error <66063257+johndoe-error@users.noreply.github.com>
2020-06-04 21:05:36 +0300

    Code Utils

--- base_64.py
+++ base_64.py
@@ -19,6 +19,7 @@ while mode != "1" or mode != "2":
             message_bytes = message.encode('UTF-8')
             base64_bytes = base64.b64encode(message_bytes)
             base64_message = base64_bytes.decode('UTF-8')
+            # encode, decode, вообще бойкот...
             print(base64_message)
             
         if mode == "2":
@@ -26,6 +27,8 @@ while mode != "1" or mode != "2":
             base64_bytes = base64_message.encode('UTF-8')
             message_bytes = base64.b64decode(base64_bytes)
             message = message_bytes.decode('UTF-8')
+           # encode, decode, вообще бойкот...
             print(message)           
     else:
         print("Неправильный режим выбран. Попробуйте еще раз!")
+# Бвъуцдгдуеп, батцхъдцэн! Юашцйн яъицфа яц бъгсдн @Mieri_1, ида-да увахц "Р батцхъэ!" Ь гашсэцяъп, дцтр атюсяеэъ... Уцхн ода багэцхярр, с щясиъд, гсюср гэашяср бсгжсэьс :) Яе ъэъ р дсьаы фсх, жцж :)
